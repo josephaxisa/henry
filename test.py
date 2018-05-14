2ff79966eeeb9d0bcec1b63bd8d8e99ab8c616e4
jcarah <jesse.carah@looker.com>
2018-05-14 10:53:08 -0400

    added main functions for schema builder and a few others

--- test.py
+++ test.py
@@ -3,8 +3,7 @@ import json
 from lookerapi import LookerApi
 from datetime import datetime
 from pprint import pprint
-from collections import defaultdict
-from itertools import groupby
+from collections import OrderedDict
 
 ### ------- HERE ARE PARAMETERS TO CONFIGURE -------
 
@@ -12,8 +11,6 @@ from itertools import groupby
 host = 'sandbox'
 # model that you wish to analyze
 model_name = 'trace_surfing'
-# How far you wish to look back
-timeframe = '28 days'
 
 ### ------- OPEN THE CONFIG FILE and INSTANTIATE API -------
 f = open('config.yml')
@@ -35,28 +32,25 @@ def get_explores(model):
     model = looker.get_model(model)
     explore_names = [i['name'] for i in model['explores']]
     explores = [looker.get_explore(model_name, i) for i in explore_names]
-    return explores
+    pprint(explores)
+    return(explores)
+
 
 def get_fields(model):
-    fields =[]
-    for explore in get_explores(model):
-        [fields.append(dimension['name']) for dimension in explore['fields']['dimensions']]
-        [fields.append(measure['name']) for measure in explore['fields']['measures']]
-    distinct_fields = sorted(set(fields))
-    return(fields)
-
-get_fields(model_name)
-def schema_builder(model):
     schema = []
-    distinct_fields = sorted(set(get_fields(model)))
-    view_field_pairs = [field.split('.') for field in distinct_fields]
-    for key, group in groupby(view_field_pairs, lambda x:x[0]):
-        schema.append({"view": key,
-        "fields": [i[1] for i in list(group)]
-        })
-    pprint(schema)
-    # print(len(distinct_fields))
-schema_builder(model_name)
-# get_fields(model_name)
-
-        # schema.append(data)
+    for explore in get_explores(model):
+        fields = []
+        explore_name = explore['name']
+        fields.append([dimension['name'] for dimension in explore['fields']['dimensions']])
+        fields.append([measure['name'] for measure in explore['fields']['measures']])
+        fields.append([filter['name'] for filter in explore['fields']['filters']])
+        schema.append(
+        {'explore': explore_name,
+         'fields':fields
+         # 'dimensions': dimensions,
+         # 'measures' : measures,
+         # 'fitlers' : filters
+        }
+        )
+    # pprint(schema)
+get_fields(model_name)
