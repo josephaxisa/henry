a7050befe13482533c4fef96996624281fe960eb
jax <josephaxisa@gmail.com>
2018-07-25 16:30:56 +0100

    latest build and source

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ URL = "https://github.com/llooker/lookml_field_usage"
 EMAIL = 'jax@looker.com'
 AUTHOR = 'Joseph Axisa'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.0.1.b1'
+VERSION = '0.0.1.b2'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
